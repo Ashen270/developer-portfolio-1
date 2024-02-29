cd3feaa9778be48a7b4590a6ebe8688c6cfcf333
Ashen Shamilka Wickramasinghe <ashenshamilka270@gmail.com>
2024-02-29 14:13:37 +0530

    updateheaderdata

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -3,7 +3,7 @@ export const headerData = {
     name: 'Ashen Shamilka',
     title: "Hello! I am",
     desciption: "Hello! I am Ashen, a professional and passionate programmer in my daily life. A quick learner with a self-learning attitude. I love to learn and explore new technologies and am Passionate about Problem Solving. ",
-    image: 'https://i.ibb.co/ZmdN6x/abu-said.jpg',
+    image: 'https://drive.google.com/file/d/1_9VoYsrLVh2yyW94mgv3I3LctYVV0DYp/view?usp=drive_link',
     imagebw: 'https://i.ibb.co/Cw2Xp90/abu-said-bw.jpg',
     resumePdf: 'https://docs.google.com/document/d/1MkkoRX98FS47CaEyeodyPzi4OkW8SH7Gv55f4MQkHV8/edit?usp=sharing'
 }
